e8f24785b6d17667ea7b87a20184be830aebb3c4
Ariel Carvalho <ariell_carvalho@hotmail.com>
2024-03-16 09:21:28 -0300

    1 - Setup inicial do Projeto / 2 - Setup inicial do projeto - MySQL

---
+++ src/main/java/br/com/alura_senac/farmacia_api_rest/FarmaciaApiRestApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura_senac.farmacia_api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaApiRestApplication.class, args);
+	}
+
+}
