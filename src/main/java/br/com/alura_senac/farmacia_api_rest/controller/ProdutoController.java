a667cafb64f6388e2cf5bd9bb5cd53dd8932488d
Ariel Carvalho <ariell_carvalho@hotmail.com>
2024-03-18 18:39:55 -0300

    Alterando os métodos para ResponseEntity para receber os códigos corretos nas requisições

--- src/main/java/br/com/alura_senac/farmacia_api_rest/controller/ProdutoController.java
+++ src/main/java/br/com/alura_senac/farmacia_api_rest/controller/ProdutoController.java
@@ -2,6 +2,7 @@ package br.com.alura_senac.farmacia_api_rest.controller;
 
 import br.com.alura_senac.farmacia_api_rest.DTO.DadosAtualizacaoProduto;
 import br.com.alura_senac.farmacia_api_rest.DTO.DadosCadastroProduto;
+import br.com.alura_senac.farmacia_api_rest.DTO.DadosDetalhamentoProduto;
 import br.com.alura_senac.farmacia_api_rest.DTO.DadosListagemProduto;
 import br.com.alura_senac.farmacia_api_rest.repository.ProdutoRepository;
 import br.com.alura_senac.farmacia_api_rest.modelo.Produto;
@@ -10,10 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("produtos")
@@ -24,28 +26,37 @@ public class ProdutoController
 
     @PostMapping
     @Transactional
-    public void cadastrarProduto(@RequestBody @Valid DadosCadastroProduto dadosCadastroProduto)
+    public ResponseEntity cadastrarProduto(@RequestBody @Valid DadosCadastroProduto dadosCadastroProduto, UriComponentsBuilder uriComponentsBuilder)
     {
+        var produto = new Produto(dadosCadastroProduto);
         produtoRepository.save(new Produto(dadosCadastroProduto));
+
+        var uri = uriComponentsBuilder.path("/produtos/{id}").buildAndExpand(produto.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DadosDetalhamentoProduto(produto));
     }
 
     @GetMapping
-    public Page<DadosListagemProduto> listar(@PageableDefault(size = 1, sort = {"nome"}) Pageable paginacao)
+    public ResponseEntity<Page<DadosListagemProduto>> listar(@PageableDefault(size = 1, sort = {"nome"}) Pageable paginacao)
     {
-        return produtoRepository.findAll(paginacao).map(DadosListagemProduto::new);
+        var page = produtoRepository.findAll(paginacao).map(DadosListagemProduto::new);
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoProduto dadosAtualizacaoProduto)
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoProduto dadosAtualizacaoProduto)
     {
         var produto = produtoRepository.getReferenceById(dadosAtualizacaoProduto.id());
         produto.atualizarInformacoes(dadosAtualizacaoProduto);
+        return ResponseEntity.ok(new DadosDetalhamentoProduto(produto));
     }
 
     @DeleteMapping("/{id}")
-    public void excluir(@PathVariable Long id)
+    @Transactional
+    public ResponseEntity excluir(@PathVariable Long id)
     {
         produtoRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
